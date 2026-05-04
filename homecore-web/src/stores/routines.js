a78ed1a48cf0988d9fb1692d85b5ba7fdb3d6f30
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-05-04 18:45:49 -0300

    feat: store of routines

--- homecore-web/src/stores/routines.js
+++ homecore-web/src/stores/routines.js
@@ -1,18 +1,56 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
-
-// Store de rutinas
+import { ref, computed } from 'vue'
+import * as api from '@/services/api'
 
 export const useRoutinesStore = defineStore('routines', () => {
-  // const routines = ref([])
-  // const loading = ref(false)
+  const routines = ref([])
+  const loading = ref(false)
+  const error = ref(null)
 
-  // const favoriteRoutines = computed(() => routines.value.filter(r => r.isFavorite))
+  const favoriteRoutines = computed(() => routines.value.filter(r => r.isFavorite))
 
-  // async function fetchRoutines() { /* api.getRoutines() */ }
-  // async function execute(routineId) { /* api.executeRoutine() */ }
-  // async function create(data) { /* api.createRoutine() */ }
-  // async function remove(routineId) { /* api.deleteRoutine() */ }
+  function getById(id) {
+    return routines.value.find(r => String(r.id) === String(id))
+  }
 
-  return {}
+  async function fetchRoutines() {
+    loading.value = true
+    error.value = null
+    try {
+      routines.value = await api.getRoutines()
+    } catch (e) {
+      error.value = e.message
+    } finally {
+      loading.value = false
+    }
+  }
+
+  async function execute(id) {
+    await api.executeRoutine(id)
+  }
+
+  async function create(data) {
+    const routine = await api.createRoutine(data)
+    routines.value.push(routine)
+  }
+
+  async function update(id, data) {
+    const updated = await api.updateRoutine(id, data)
+    const idx = routines.value.findIndex(r => String(r.id) === String(id))
+    if (idx !== -1) routines.value[idx] = { ...routines.value[idx], ...updated }
+  }
+
+  async function remove(id) {
+    await api.deleteRoutine(id)
+    routines.value = routines.value.filter(r => String(r.id) !== String(id))
+  }
+
+  async function toggleFavorite(id) {
+    const routine = routines.value.find(r => String(r.id) === String(id))
+    if (!routine) return
+    await api.updateRoutine(id, { isFavorite: !routine.isFavorite })
+    routine.isFavorite = !routine.isFavorite
+  }
+
+  return { routines, loading, error, favoriteRoutines, getById, fetchRoutines, execute, create, update, remove, toggleFavorite }
 })
