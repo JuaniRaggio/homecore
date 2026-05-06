4ad793361843ad7896c5f3a1959de28953b27eec
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-05-06 14:32:18 -0300

    feat: rooms.js impl

--- homecore-web/src/services/api/rooms.js
+++ homecore-web/src/services/api/rooms.js
@@ -1,2 +1,7 @@
 import { request } from './client'
 
+export const getRooms = (homeId) => request('GET', `/homes/${homeId}/rooms`)
+export const getRoom = (id) => request('GET', `/rooms/${id}`)
+export const createRoom = (homeId, data) => request('POST', `/homes/${homeId}/rooms`, data)
+export const updateRoom = (id, data) => request('PUT', `/rooms/${id}`, data)
+export const deleteRoom = (id) => request('DELETE', `/rooms/${id}`)
