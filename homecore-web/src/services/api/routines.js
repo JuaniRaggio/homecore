4d2648fdf52fd6f2fe5f5ad231350428d0f32513
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-05-06 14:32:24 -0300

    feat: routines.js impl

--- homecore-web/src/services/api/routines.js
+++ homecore-web/src/services/api/routines.js
@@ -1,2 +1,8 @@
 import { request } from './client'
 
+export const getRoutines = () => request('GET', '/routines')
+export const getRoutine = (id) => request('GET', `/routines/${id}`)
+export const createRoutine = (data) => request('POST', '/routines', data)
+export const updateRoutine = (id, data) => request('PUT', `/routines/${id}`, data)
+export const deleteRoutine = (id) => request('DELETE', `/routines/${id}`)
+export const executeRoutine = (id) => request('PUT', `/routines/${id}/execute`)
