47b2289c9235eb524b7588472c10df01187842c2
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-05-06 14:32:05 -0300

    feat: homes.js impl

--- homecore-web/src/services/api/homes.js
+++ homecore-web/src/services/api/homes.js
@@ -1,2 +1,7 @@
 import { request } from './client'
 
+export const getHomes = () => request('GET', '/homes')
+export const getHome = (id) => request('GET', `/homes/${id}`)
+export const createHome = (data) => request('POST', '/homes', data)
+export const updateHome = (id, data) => request('PUT', `/homes/${id}`, data)
+export const deleteHome = (id) => request('DELETE', `/homes/${id}`)
