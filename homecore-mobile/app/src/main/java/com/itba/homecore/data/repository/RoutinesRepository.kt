6eae9bcf48daf331daf2de6e219c486aaaca3f70
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-06-12 20:35:02 -0300

    feat: add delete routine repo

--- homecore-mobile/app/src/main/java/com/itba/homecore/data/repository/RoutinesRepository.kt
+++ homecore-mobile/app/src/main/java/com/itba/homecore/data/repository/RoutinesRepository.kt
@@ -3,11 +3,12 @@ package com.itba.homecore.data.repository
 import com.itba.homecore.data.model.Routine
 
 /**
- * Contrato de la capa de datos de rutinas. Ver [DevicesRepository] para el patrón.
+ * Data-layer contract for routines. See [DevicesRepository] for the pattern.
  */
 interface RoutinesRepository {
     suspend fun getRoutines(): Result<List<Routine>>
     suspend fun executeRoutine(id: String): Result<Unit>
     suspend fun toggleFavorite(routine: Routine): Result<Routine>
     suspend fun toggleActive(routine: Routine): Result<Routine>
+    suspend fun deleteRoutine(id: String): Result<Unit>
 }
