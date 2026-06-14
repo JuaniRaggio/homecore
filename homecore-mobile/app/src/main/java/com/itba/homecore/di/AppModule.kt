74d65f7224c15bfbf35deece97a6d89795ac89b7
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-06-14 00:44:40 -0300

    chore: add homes repo

--- homecore-mobile/app/src/main/java/com/itba/homecore/di/AppModule.kt
+++ homecore-mobile/app/src/main/java/com/itba/homecore/di/AppModule.kt
@@ -3,8 +3,10 @@ package com.itba.homecore.di
 import android.content.Context
 import com.itba.homecore.data.repository.AuthRepository
 import com.itba.homecore.data.repository.DevicesRepository
+import com.itba.homecore.data.repository.HomesRepository
 import com.itba.homecore.data.repository.RemoteAuthRepository
 import com.itba.homecore.data.repository.RemoteDevicesRepository
+import com.itba.homecore.data.repository.RemoteHomesRepository
 import com.itba.homecore.data.repository.RemoteRoutinesRepository
 import com.itba.homecore.data.repository.RoutinesRepository
 
@@ -16,6 +18,10 @@ object AppModule {
 
     val routinesRepository: RoutinesRepository by lazy {
         RemoteRoutinesRepository()
+    }
+
+    val homesRepository: HomesRepository by lazy {
+        RemoteHomesRepository()
     }
 
     fun authRepository(context: Context): AuthRepository =
